675a9562969e2ee128cdda367ee19535c6ca01e2
jarvispact <jarvispact@gmail.com>
2019-03-02 23:24:47 +0100

    remove spec-files and comments

--- utils/shader-utils.js
+++ utils/shader-utils.js
@@ -41,8 +41,7 @@ export const getBlinnPhongShaderSource = (rendererArguments) => {
             vUv = uv;
             vNormal = normalMatrix * normal;
             vPosition = (modelMatrix * vec4( position, 1.0 )).xyz;
-            // gl_Position = mvp * vec4(position, 1.0);
-            gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+            gl_Position = mvp * vec4(position, 1.0);
         }
     `;
 
@@ -53,20 +52,13 @@ export const getBlinnPhongShaderSource = (rendererArguments) => {
 
         ${getUniformsDeclaration(fUniforms)}
 
-        // This is needed to figure out the specular highlight
         vec3 cameraPosition;
-        // The model color
         vec4 color;
-        // The model's specular color
         vec4 specularColor;
-        // The amount of ambient light
         float ambientLight;
-        
-        // The specular values passed in from the shader
         float specularAmount;
         float specularShininess;
         
-        // The lights
         vec3 light;
 
         out vec4 fragmentColor;
@@ -80,34 +72,20 @@ export const getBlinnPhongShaderSource = (rendererArguments) => {
             specularShininess = 50.0;
             light = normalize(vec3(0.0, 5.0, 10.0));
 
-            // Get the direction from the surface to the camera, which is located at
-            // (0,-1,0) in clip space. The camera position is hardcoded for convenience.
             vec3 directionToCamera = normalize(cameraPosition - vPosition);
-
-            // Find the vector pointing halfway between the directions
-            // of the camera and light from this point on the surface
             vec3 halfwayVector = normalize( directionToCamera + light );
 
-            // Calculate the specular highlight
             float specularBrightness = (
-              
-              specularAmount * // Adjust the overall amount of specular light
-              // Find the angle between the normal and the halfwayVector by taking
-              // the dot product, and then raise it to a certain power. The power
-              // increases or decreases the size of the bright spot.
+              specularAmount *
               pow(
                 max(0.0, dot(vNormal, halfwayVector)),
                 specularShininess
               )
             );
             
-            // Calculate the brightness of the surface using the lambertian lighting model
             float lightDotProduct = dot( normalize(vNormal), light );
             float surfaceBrightness = max( 0.0, lightDotProduct );
-
-            // fragmentColor = texture(diffuseTexture, vUv);
             
-            // Multiply together all of the various light values
             fragmentColor = vec4(texture(diffuseTexture, vUv).xyz * surfaceBrightness + specularColor.xyz * specularBrightness, color.w);
         }
     `;
