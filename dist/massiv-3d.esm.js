2651ebba1b5ad662e369bd7959c5b50dd0225e50
jarvispact <jarvispact@gmail.com>
2019-12-31 14:17:15 +0100

    chore: reset and update dependencies

--- dist/massiv-3d.esm.js
+++ dist/massiv-3d.esm.js
@@ -912,162 +912,6 @@ class Scene extends Transform3D {
     }
 }
 
-/* eslint-disable prefer-destructuring */
-
-class Vec2 {
-    constructor(x = 0, y = 0) {
-        this.x = x;
-        this.y = y;
-    }
-
-    getAsArray() {
-        return [this.x, this.y];
-    }
-
-    getAsFloat32Array() {
-        return Float32Array.from(this.getAsArray());
-    }
-
-    setFromArray(vec2) {
-        this.x = vec2[0];
-        this.y = vec2[1];
-        return this;
-    }
-
-    clone() {
-        return new Vec2(this.x, this.y);
-    }
-
-    add(x, y) {
-        this.x += x;
-        this.y += y;
-        return this;
-    }
-
-    multiply(x, y) {
-        this.x *= x;
-        this.y *= y;
-        return this;
-    }
-}
-
-const useMtlRegex = /^usemtl\s(\S+)$/;
-const objNameRegex = /^o\s(\S+)$/;
-const vertexRegex = /^v\s(\S+)\s(\S+)\s(\S+)$/;
-const normalRegex = /^vn\s(\S+)\s(\S+)\s(\S+)$/;
-const uvRegex = /^vt\s(\S+)\s(\S+)$/;
-const triangleFaceRegex = /^f\s(\S+)\s(\S+)\s(\S+)$/;
-const triangleVertexRegex = /^(\d+)\/(\d+)\/(\d+)$/;
-
-const parse = (objFileString) => {
-    console.log(objFileString.trim());
-    const lines = objFileString.trim().split('\n');
-
-    const allVertices = [];
-    const allNormals = [];
-    const allUvs = [];
-
-    // parse shared vertices, normals and uvs
-    lines.forEach((line) => {
-        const vertexMatch = line.match(vertexRegex);
-        if (vertexMatch) {
-            const [, x, y, z] = vertexMatch;
-            allVertices.push(new Vec3(x, y, z));
-        }
-
-        const normalMatch = line.match(normalRegex);
-        if (normalMatch) {
-            const [, x, y, z] = normalMatch;
-            allNormals.push(new Vec3(x, y, z));
-        }
-
-        const uvMatch = line.match(uvRegex);
-        if (uvMatch) {
-            const [, x, y] = uvMatch;
-            allUvs.push(new Vec2(x, y));
-        }
-    });
-
-    console.log({ allVertices, allNormals, allUvs });
-
-
-    // parse objects with materials
-    const objects = [];
-    let currentObjectIndex = 0;
-    let currentMaterialIndex = 0;
-
-    lines.forEach((line) => {
-        const objMatch = line.match(objNameRegex);
-        if (objMatch) {
-            const [, objName] = objMatch;
-
-            objects[currentObjectIndex] = {
-                name: objName,
-                vertices: [],
-                normals: [],
-                uvs: [],
-                materials: [],
-            };
-
-            currentObjectIndex++;
-        }
-
-        const lastObjIndex = currentObjectIndex - 1;
-
-        const useMtlMatch = line.match(useMtlRegex);
-        if (useMtlMatch) {
-            const [, materialName] = useMtlMatch;
-
-            objects[lastObjIndex].materials[currentMaterialIndex] = {
-                name: materialName,
-                indices: [],
-            };
-
-            currentMaterialIndex++;
-        }
-
-        const lastMaterialIndex = currentMaterialIndex - 1;
-
-        const triangleFaceMatch = line.match(triangleFaceRegex);
-        if (triangleFaceMatch) {
-            const [, one, two, three] = triangleFaceMatch;
-            const oneMatch = one.match(triangleVertexRegex);
-            if (oneMatch) {
-                const [, vertexIndex, uvIndex, normalIndex] = oneMatch;
-                console.log('one', { vertexIndex, uvIndex, normalIndex });
-                const realVertexIndex = vertexIndex - 1;
-                const realNormalIndex = normalIndex - 1;
-                const realUvIndex = uvIndex - 1;
-                objects[lastObjIndex].vertices.push(...allVertices[realVertexIndex].getAsArray());
-                objects[lastObjIndex].normals.push(...allNormals[realNormalIndex].getAsArray());
-                objects[lastObjIndex].uvs.push(...allUvs[realUvIndex].getAsArray());
-                objects[lastObjIndex].materials[lastMaterialIndex].indices.push(realVertexIndex);
-            }
-
-            const twoMatch = two.match(triangleVertexRegex);
-            if (twoMatch) {
-                const [, vertexIndex, uvIndex, normalIndex] = twoMatch;
-                console.log('two', { vertexIndex, uvIndex, normalIndex });
-            }
-
-            const threeMatch = three.match(triangleVertexRegex);
-            if (threeMatch) {
-                const [, vertexIndex, uvIndex, normalIndex] = threeMatch;
-                console.log('three', { vertexIndex, uvIndex, normalIndex });
-            }
-        }
-    });
-
-    console.log({ objects });
-
-    return objects;
-};
-
-
-const ObjLoader = {
-    load: (objFilePath) => fetch(objFilePath).then(response => response.text()).then(parse),
-};
-
 /* eslint-disable prefer-destructuring, one-var, one-var-declaration-per-line */
 
 class Mat3 {
@@ -1212,6 +1056,45 @@ class Mat3 {
 
     static normalMatrixFromMat4(mat4) {
         return new Mat3().setAsNormalMatrixFromMat4(mat4);
+    }
+}
+
+/* eslint-disable prefer-destructuring */
+
+class Vec2 {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+
+    getAsArray() {
+        return [this.x, this.y];
+    }
+
+    getAsFloat32Array() {
+        return Float32Array.from(this.getAsArray());
+    }
+
+    setFromArray(vec2) {
+        this.x = vec2[0];
+        this.y = vec2[1];
+        return this;
+    }
+
+    clone() {
+        return new Vec2(this.x, this.y);
+    }
+
+    add(x, y) {
+        this.x += x;
+        this.y += y;
+        return this;
+    }
+
+    multiply(x, y) {
+        this.x *= x;
+        this.y *= y;
+        return this;
     }
 }
 
@@ -1709,4 +1592,4 @@ class WebGl2Renderer {
     }
 }
 
-export { DirectionalLight, Geometry, Mat3, Mat4, Mesh, Node, ObjLoader, OrthographicCamera, PerspectiveCamera, Quat, Scene, StandardMaterial, Transform3D, Vec2, Vec3, Vec4, Viewport, WebGl2Renderer as WebGL2Renderer };
+export { DirectionalLight, Geometry, Mat3, Mat4, Mesh, Node, OrthographicCamera, PerspectiveCamera, Quat, Scene, StandardMaterial, Transform3D, Vec2, Vec3, Vec4, Viewport, WebGl2Renderer as WebGL2Renderer };
--- dist/massiv-3d.umd.js
+++ dist/massiv-3d.umd.js
@@ -2,7 +2,7 @@
     typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
     typeof define === 'function' && define.amd ? define(['exports'], factory) :
     (global = global || self, factory(global.MASSIV = {}));
-}(this, function (exports) { 'use strict';
+}(this, (function (exports) { 'use strict';
 
     /* eslint-disable no-bitwise, no-nested-ternary, no-mixed-operators */
 
@@ -918,162 +918,6 @@
         }
     }
 
-    /* eslint-disable prefer-destructuring */
-
-    class Vec2 {
-        constructor(x = 0, y = 0) {
-            this.x = x;
-            this.y = y;
-        }
-
-        getAsArray() {
-            return [this.x, this.y];
-        }
-
-        getAsFloat32Array() {
-            return Float32Array.from(this.getAsArray());
-        }
-
-        setFromArray(vec2) {
-            this.x = vec2[0];
-            this.y = vec2[1];
-            return this;
-        }
-
-        clone() {
-            return new Vec2(this.x, this.y);
-        }
-
-        add(x, y) {
-            this.x += x;
-            this.y += y;
-            return this;
-        }
-
-        multiply(x, y) {
-            this.x *= x;
-            this.y *= y;
-            return this;
-        }
-    }
-
-    const useMtlRegex = /^usemtl\s(\S+)$/;
-    const objNameRegex = /^o\s(\S+)$/;
-    const vertexRegex = /^v\s(\S+)\s(\S+)\s(\S+)$/;
-    const normalRegex = /^vn\s(\S+)\s(\S+)\s(\S+)$/;
-    const uvRegex = /^vt\s(\S+)\s(\S+)$/;
-    const triangleFaceRegex = /^f\s(\S+)\s(\S+)\s(\S+)$/;
-    const triangleVertexRegex = /^(\d+)\/(\d+)\/(\d+)$/;
-
-    const parse = (objFileString) => {
-        console.log(objFileString.trim());
-        const lines = objFileString.trim().split('\n');
-
-        const allVertices = [];
-        const allNormals = [];
-        const allUvs = [];
-
-        // parse shared vertices, normals and uvs
-        lines.forEach((line) => {
-            const vertexMatch = line.match(vertexRegex);
-            if (vertexMatch) {
-                const [, x, y, z] = vertexMatch;
-                allVertices.push(new Vec3(x, y, z));
-            }
-
-            const normalMatch = line.match(normalRegex);
-            if (normalMatch) {
-                const [, x, y, z] = normalMatch;
-                allNormals.push(new Vec3(x, y, z));
-            }
-
-            const uvMatch = line.match(uvRegex);
-            if (uvMatch) {
-                const [, x, y] = uvMatch;
-                allUvs.push(new Vec2(x, y));
-            }
-        });
-
-        console.log({ allVertices, allNormals, allUvs });
-
-
-        // parse objects with materials
-        const objects = [];
-        let currentObjectIndex = 0;
-        let currentMaterialIndex = 0;
-
-        lines.forEach((line) => {
-            const objMatch = line.match(objNameRegex);
-            if (objMatch) {
-                const [, objName] = objMatch;
-
-                objects[currentObjectIndex] = {
-                    name: objName,
-                    vertices: [],
-                    normals: [],
-                    uvs: [],
-                    materials: [],
-                };
-
-                currentObjectIndex++;
-            }
-
-            const lastObjIndex = currentObjectIndex - 1;
-
-            const useMtlMatch = line.match(useMtlRegex);
-            if (useMtlMatch) {
-                const [, materialName] = useMtlMatch;
-
-                objects[lastObjIndex].materials[currentMaterialIndex] = {
-                    name: materialName,
-                    indices: [],
-                };
-
-                currentMaterialIndex++;
-            }
-
-            const lastMaterialIndex = currentMaterialIndex - 1;
-
-            const triangleFaceMatch = line.match(triangleFaceRegex);
-            if (triangleFaceMatch) {
-                const [, one, two, three] = triangleFaceMatch;
-                const oneMatch = one.match(triangleVertexRegex);
-                if (oneMatch) {
-                    const [, vertexIndex, uvIndex, normalIndex] = oneMatch;
-                    console.log('one', { vertexIndex, uvIndex, normalIndex });
-                    const realVertexIndex = vertexIndex - 1;
-                    const realNormalIndex = normalIndex - 1;
-                    const realUvIndex = uvIndex - 1;
-                    objects[lastObjIndex].vertices.push(...allVertices[realVertexIndex].getAsArray());
-                    objects[lastObjIndex].normals.push(...allNormals[realNormalIndex].getAsArray());
-                    objects[lastObjIndex].uvs.push(...allUvs[realUvIndex].getAsArray());
-                    objects[lastObjIndex].materials[lastMaterialIndex].indices.push(realVertexIndex);
-                }
-
-                const twoMatch = two.match(triangleVertexRegex);
-                if (twoMatch) {
-                    const [, vertexIndex, uvIndex, normalIndex] = twoMatch;
-                    console.log('two', { vertexIndex, uvIndex, normalIndex });
-                }
-
-                const threeMatch = three.match(triangleVertexRegex);
-                if (threeMatch) {
-                    const [, vertexIndex, uvIndex, normalIndex] = threeMatch;
-                    console.log('three', { vertexIndex, uvIndex, normalIndex });
-                }
-            }
-        });
-
-        console.log({ objects });
-
-        return objects;
-    };
-
-
-    const ObjLoader = {
-        load: (objFilePath) => fetch(objFilePath).then(response => response.text()).then(parse),
-    };
-
     /* eslint-disable prefer-destructuring, one-var, one-var-declaration-per-line */
 
     class Mat3 {
@@ -1218,6 +1062,45 @@
 
         static normalMatrixFromMat4(mat4) {
             return new Mat3().setAsNormalMatrixFromMat4(mat4);
+        }
+    }
+
+    /* eslint-disable prefer-destructuring */
+
+    class Vec2 {
+        constructor(x = 0, y = 0) {
+            this.x = x;
+            this.y = y;
+        }
+
+        getAsArray() {
+            return [this.x, this.y];
+        }
+
+        getAsFloat32Array() {
+            return Float32Array.from(this.getAsArray());
+        }
+
+        setFromArray(vec2) {
+            this.x = vec2[0];
+            this.y = vec2[1];
+            return this;
+        }
+
+        clone() {
+            return new Vec2(this.x, this.y);
+        }
+
+        add(x, y) {
+            this.x += x;
+            this.y += y;
+            return this;
+        }
+
+        multiply(x, y) {
+            this.x *= x;
+            this.y *= y;
+            return this;
         }
     }
 
@@ -1721,7 +1604,6 @@
     exports.Mat4 = Mat4;
     exports.Mesh = Mesh;
     exports.Node = Node;
-    exports.ObjLoader = ObjLoader;
     exports.OrthographicCamera = OrthographicCamera;
     exports.PerspectiveCamera = PerspectiveCamera;
     exports.Quat = Quat;
@@ -1736,4 +1618,4 @@
 
     Object.defineProperty(exports, '__esModule', { value: true });
 
-}));
+})));
