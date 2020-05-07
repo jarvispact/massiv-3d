e754021cbcfa113bb2bcf7aebe10469aa7d91ab2
jarvispact <jarvispact@gmail.com>
2020-05-07 16:17:25 +0200

    remove typescript from examples

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,6 @@ import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 
-const pages = [
-    '00-hello-world',
-];
-
 export default [
     {
         input: 'src/index.ts',
@@ -29,23 +25,4 @@ export default [
             typescript(),
         ],
     },
-    ...pages.map(page => ({
-        input: `examples/${page}/index.ts`,
-        output: {
-            name: 'MASSIV',
-            file: `examples/${page}/bundle.js`,
-            format: 'umd',
-        },
-        plugins: [
-            resolve(),
-            typescript({
-                tsconfigOverride: {
-                    compilerOptions: {
-                        noEmit: true,
-                        declaration: false,
-                    },
-                },
-            }),
-        ],
-    })),
 ];
