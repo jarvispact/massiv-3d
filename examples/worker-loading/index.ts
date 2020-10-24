45b9ef512449edeca1c237fa1dc3b097f83ccedb
jarvispact <jarvispact@gmail.com>
2020-10-24 21:46:56 +0200

    remove unnessesary async iife

--- examples/worker-loading/index.ts
+++ examples/worker-loading/index.ts
@@ -13,79 +13,70 @@ const start = Date.now();
 
 const world = new World();
 
-const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+const worker = new Worker('./worker.bundle.js');
 
-(async () => {
-    const worker = new Worker('./worker.bundle.js');
+worker.onmessage = (event: WorkerEventMap['message']) => {
+    const sab = event.data.buffer;
+    const positions = new Float32Array(sab, event.data.positions.offset, event.data.positions.length);
+    const normals = new Float32Array(sab, event.data.normals.offset, event.data.normals.length);
+    const uvs = new Float32Array(sab, event.data.uvs.offset, event.data.uvs.length);
+    const indices = new Uint32Array(sab, event.data.indices.offset, event.data.indices.length);
 
-    worker.onmessage = (event: WorkerEventMap['message']) => {
-        const sab = event.data.buffer;
-        const positions = new Float32Array(sab, event.data.positions.offset, event.data.positions.length);
-        const normals = new Float32Array(sab, event.data.normals.offset, event.data.normals.length);
-        const uvs = new Float32Array(sab, event.data.uvs.offset, event.data.uvs.length);
-        const indices = new Uint32Array(sab, event.data.indices.offset, event.data.indices.length);
+    const earth = new Entity('Test', [
+        new Transform({ translation: [-20, 0, 0] }),
+        new Geometry({ positions, normals, uvs, indices }),
+    ]);
 
-        const earth = new Entity('Test', [
-            new Transform({ translation: [-20, 0, 0] }),
-            new Geometry({ positions, normals, uvs, indices }),
-        ]);
+    world.addEntity(earth);
+    const end = Date.now();
+    console.log('finish: ', (end - start) / 1000);
+};
 
-        world.addEntity(earth);
-        const end = Date.now();
-        console.log('finish: ', (end - start) / 1000);
-    };
+worker.postMessage('load-models');
 
-    worker.postMessage('load-models');
+const worker2 = new Worker('./worker2.bundle.js');
 
-    const worker2 = new Worker('./worker2.bundle.js');
+worker2.onmessage = (event: WorkerEventMap['message']) => {
+    const sab = event.data.buffer;
+    const positions = new Float32Array(sab, event.data.positions.offset, event.data.positions.length);
+    const normals = new Float32Array(sab, event.data.normals.offset, event.data.normals.length);
+    const uvs = new Float32Array(sab, event.data.uvs.offset, event.data.uvs.length);
+    const indices = new Uint32Array(sab, event.data.indices.offset, event.data.indices.length);
 
-    worker2.onmessage = (event: WorkerEventMap['message']) => {
-        const sab = event.data.buffer;
-        const positions = new Float32Array(sab, event.data.positions.offset, event.data.positions.length);
-        const normals = new Float32Array(sab, event.data.normals.offset, event.data.normals.length);
-        const uvs = new Float32Array(sab, event.data.uvs.offset, event.data.uvs.length);
-        const indices = new Uint32Array(sab, event.data.indices.offset, event.data.indices.length);
+    const earth = new Entity('Test2', [
+        new Transform({ translation: [20, 0, 0] }),
+        new Geometry({ positions, normals, uvs, indices }),
+    ]);
 
-        const earth = new Entity('Test2', [
-            new Transform({ translation: [20, 0, 0] }),
-            new Geometry({ positions, normals, uvs, indices }),
-        ]);
+    world.addEntity(earth);
+    const end = Date.now();
+    console.log('finish: ', (end - start) / 1000);
+};
 
-        world.addEntity(earth);
-        const end = Date.now();
-        console.log('finish: ', (end - start) / 1000);
-    };
+worker2.postMessage('load-models');
 
-    worker2.postMessage('load-models');
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-    const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+const mouseInput = new MouseInput(canvas);
 
-    const mouseInput = new MouseInput(canvas);
+const gl = getWebgl2Context(canvas);
 
-    const gl = getWebgl2Context(canvas);
+const camera = createPerspectiveCamera(gl, [0, 50, 200], canvas.width / canvas.height);
 
-    const camera = createPerspectiveCamera(gl, [0, 50, 200], canvas.width / canvas.height);
+const renderSystem = createRenderSystem(gl, world, {
+    onCacheEntity: (shaderProgram) => camera.bindToShaderProgram(shaderProgram),
+});
 
-    const renderSystem = createRenderSystem(gl, world, {
-        onCacheEntity: (shaderProgram) => camera.bindToShaderProgram(shaderProgram),
-    });
+world.addSystem(renderSystem);
+world.addSystem(createTrackballCameraSystem(camera, mouseInput));
 
-    world.addSystem(renderSystem);
-    world.addSystem(createTrackballCameraSystem(camera, mouseInput));
+window.addEventListener('resize', () => {
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    camera.setAspect(canvas.width / canvas.height);
+});
 
-    window.addEventListener('resize', () => {
-        canvas.width = canvas.clientWidth;
-        canvas.height = canvas.clientHeight;
-        gl.viewport(0, 0, canvas.width, canvas.height);
-        camera.setAspect(canvas.width / canvas.height);
-    });
-
-    runGameLoop(world);
-
-    // for (let i = 0; i < 100; i++) {
-    //     console.log('doing some more important work', i);
-    //     await sleep(100);
-    // }
-})();
+runGameLoop(world);
