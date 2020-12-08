45ad719f719e3c10a938df092d8169aa35c4c0af
jarvispact <jarvispact@gmail.com>
2020-12-08 10:38:50 +0100

    remove unused import

--- examples/pong/misc.ts
+++ examples/pong/misc.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { vec3 } from 'gl-matrix';
-import { Component, Entity, UBO } from '../../src';
+import { Component, UBO } from '../../src';
 import { PerspectiveCamera } from './camera/perspective-camera';
 
 export const randomNegative = (val: number) => Math.round(Math.random()) === 0 ? -val : val;
