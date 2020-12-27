3ea89da22fb7891c7d1a6487407cd783051dbfb0
jarvispact <jarvispact@gmail.com>
2020-12-27 17:17:04 +0100

    update dist folder

--- dist/systems/trackball-camera-control-system.d.ts
+++
@@ -1,8 +0,0 @@
-import { System } from '../ecs/system';
-import { World } from '../ecs/world';
-declare type CameraControlArgs = {
-    world: World;
-    canvas: HTMLCanvasElement;
-};
-export declare const createTrackballCameraControlSystem: ({ world, canvas }: CameraControlArgs) => System;
-export {};
--- dist/systems/webgl-2-rendering-system.d.ts
+++
@@ -1,9 +0,0 @@
-import { System } from '../ecs/system';
-import { World } from '../ecs/world';
-declare type Webgl2RenderingSystemArgs = {
-    world: World;
-    canvas: HTMLCanvasElement;
-    maxDirectionalLights?: number;
-};
-export declare const createWebgl2RenderingSystem: ({ world, canvas, maxDirectionalLights }: Webgl2RenderingSystemArgs) => System;
-export {};
